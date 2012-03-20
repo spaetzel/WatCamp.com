4e1c9bb8e568224da421ad3821cb08f1dce748ea
Will Spaetzel <will@spaetzel.com>
2012-03-19 22:08:11 -0400

    Fix for Firefox

--- js/views/events/list.js
+++ js/views/events/list.js
@@ -7,13 +7,13 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/events/list.html'
       this.template = _.template(listTemplate);
     },
     render: function() {
-      var eventsUrl = "http://pipes.yahoo.com/pipes/pipe.run?_id=c4d622b74864444d50c1d73e56bfbb13&_render=json";
+      var eventsUrl = "http://pipes.yahoo.com/pipes/pipe.run?_id=c4d622b74864444d50c1d73e56bfbb13&_render=json&_callback=?";
 
       console.log(eventsUrl);
 
       var self = this;
 
-      $.get(eventsUrl, function(data) {
+      $.getJSON(eventsUrl, function(data) {
         var events = data.value.items;
 
         console.log(events.length);
