d72805d21c0a41e40cf050d74be83adc6d5397d3
Paul Nijjar <pnijjar@alumni.uwaterloo.ca>
2017-05-21 04:50:56 -0400

    hide menu if item is clicked

--- js/views/home/hamburger-menu.js
+++ js/views/home/hamburger-menu.js
@@ -5,18 +5,27 @@ define(['jquery', 'order!underscore', 'backbone'], function($, _, Backbone) {
     el: $('.navbar'),
 
     initialize: function() { 
+      // Add a class to all menu items so we can tell when 
+      // they have been clicked.
+      $('ul.nav li').addClass('navitem');
     }, 
 
     render: function() {
     },
 
     events: { 
-      'click .btn.btn-navbar': 'toggleHamburgerMenu'
+      'click .btn.btn-navbar': 'toggleHamburgerMenu',
+      'click .navitem': 'onItemClick',
     }, 
 
     toggleHamburgerMenu: function(evt) { 
       $('.nav-collapse').toggleClass("active");
-    } // end toggleHamburgerMenu
+    }, // end toggleHamburgerMenu
+
+    onItemClick: function(evt) { 
+      // Re-hide menu if an item has been clicked
+      $('.nav-collapse').removeClass('active');
+    },
 
   });
 
