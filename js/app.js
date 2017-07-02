0a5e7e377db7a1c4a197fa7295a82d1b35819c83
Paul Nijjar <pnijjar@alumni.uwaterloo.ca>
2017-07-02 12:11:59 -0400

    Enable bootstrap library

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ define([
   'underscore',
   'backbone',
   'router', // Request router.js
-], function($, _, Backbone, Router){
+  'bootstrap',
+], function($, _, Backbone, Router, Bootstrap){
   var initialize = function(){
     // Pass in our Router module and call it's initialize function
     Router.initialize();
